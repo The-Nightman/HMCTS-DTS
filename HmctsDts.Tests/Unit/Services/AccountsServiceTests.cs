f9c0d9b624ff2123c128761a2f30928a1387c602
Keenan Brant <keenan.brant@yahoo.co.uk>
2025-04-20 14:53:07 +0100

    add AccountsServiceTests unit test class to tests project with initial test for RegisterNewCaseWorker method in AccountsSevice class

---
+++ HmctsDts.Tests/Unit/Services/AccountsServiceTests.cs
@@ -0,0 +1,56 @@
+using HmctsDts.Server.DTOs;
+using HmctsDts.Server.Entities;
+using HmctsDts.Server.Interfaces;
+using HmctsDts.Server.Services;
+using NSubstitute;
+
+namespace HmctsDts.Tests.Unit.Services;
+
+public class AccountsServiceTests
+{
+    private IUserRepository _mockUserRepository;
+    private IAccountsService _accountsService;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mockUserRepository = Substitute.For<IUserRepository>();
+        _accountsService = new AccountsService(_mockUserRepository);
+    }
+
+    /// <summary>
+    /// Verifies that the RegisterNewCaseWorker method creates a new user with the correct properties.
+    /// </summary>
+    [Test]
+    [Category("Unit")]
+    [Category("AccountsService")]
+    public async Task RegisterNewCaseWorker_CreatesUser()
+    {
+        // Arrange
+        var userObj = new RegisterUserDto
+        {
+            Name = "John Doe",
+            Email = "john.doe@test.com",
+            Password = "password"
+        };
+
+        User? capturedUser = null;
+        await _mockUserRepository.CreateUser(Arg.Do<User>(u => capturedUser = u));
+
+        // Act
+        await _accountsService.RegisterNewCaseWorker(userObj);
+
+        // Assert
+        await _mockUserRepository.Received(1).CreateUser(Arg.Any<User>());
+        
+        Assert.That(capturedUser, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(capturedUser.Name, Is.EqualTo(userObj.Name));
+            Assert.That(capturedUser.Email, Is.EqualTo(userObj.Email));
+            Assert.That(capturedUser.StaffId, Does.Match(@"^EJD-CTS-\d{4}$"));
+            Assert.That(capturedUser.Hash, Is.Not.Null);
+            Assert.That(capturedUser.Salt, Is.Not.Null);
+        });
+    }
+}
