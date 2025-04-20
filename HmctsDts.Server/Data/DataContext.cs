e0643157873408c18a6fedfb444da818f5651dee
Keenan Brant <keenan.brant@yahoo.co.uk>
2025-04-20 01:58:46 +0100

    add unique constraint to user entity email

--- HmctsDts.Server/Data/DataContext.cs
+++ HmctsDts.Server/Data/DataContext.cs
@@ -7,4 +7,11 @@ namespace HmctsDts.Server.Data;
 public class DataContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+    }
 }
