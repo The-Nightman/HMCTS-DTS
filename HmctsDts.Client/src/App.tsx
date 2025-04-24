b610275597df88d8585b3f079fa1a18d200d189b
Keenan Brant <keenan.brant@yahoo.co.uk>
2025-04-24 01:24:27 +0100

    add routes for landing page, login page and register page in app.tsx using UnauthenticatedLayout and StartPage screen

--- HmctsDts.Client/src/App.tsx
+++ HmctsDts.Client/src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Navigate, Route, Routes } from "react-router";
+import { UnauthenticatedLayout } from "./Layouts";
+import { StartPage } from "./Pages";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<UnauthenticatedLayout />}>
+          <Route
+            index
+            // We can't set /start as an entry point but we can redirect
+            element={<Navigate to={"/start"} />}
+          />
+          <Route path="/start" element={<StartPage />} />
+          <Route path="/register" element={<p>register</p>} />
+          <Route path="/login" element={<p>login</p>} />
+          <Route path="*" element={<Navigate to={"/start"} />} />
+        </Route>
+        <Route path="*" element={<div>404 Not Found</div>} />
+      </Routes>
     </>
   );
-}
+};
 
 export default App;
