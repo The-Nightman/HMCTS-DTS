07c8802ee33dc4ed1dcc87e7064e706968738a81
Keenan Brant <keenan.brant@yahoo.co.uk>
2025-04-25 02:20:52 +0100

    add jsdoc to api functions

--- HmctsDts.Client/src/api/postLoginUser.ts
+++ HmctsDts.Client/src/api/postLoginUser.ts
@@ -1,7 +1,15 @@
+import { AxiosResponse } from "axios";
 import { LoginData } from "../types/loginData";
 import { backend } from "./api";
 
-export const postLoginUser = async (formData: LoginData) => {
+/**
+ * Sends a login request to the backend API with the provided login data.
+ * 
+ * @async
+ * @param {LoginData} formData - The login credentials to be sent to the server
+ * @returns {Promise<AxiosResponse>} A promise that resolves to the server response
+ */
+export const postLoginUser = async (formData: LoginData): Promise<AxiosResponse> => {
   const res = await backend.post("/Accounts/login", formData);
   return res;
 };
